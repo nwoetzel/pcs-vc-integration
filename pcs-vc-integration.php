a69b3872ccccef0b400963ed5dbf71b389dfacef
Nils Woetzel <nils.woetzel@h-its.org>
2017-03-14 09:37:11 +0000

    added translation capability and german translation

--- pcs-vc-integration.php
+++ pcs-vc-integration.php
@@ -4,7 +4,7 @@
  * Plugin Name: Post Content Shortcodes Visual Composer Integration
  * Plugin URI:  https://github.com/nwoetzel/pcs-vc-integration
  * Description: This plugin maps post-content-shortcodes shortcodes to WPBakery Visual Composer elements.
- * Version:     1.0.1
+ * Version:     1.1.0
  * Author:      Nils Woetzel
  * Author URI:  https://github.com/nwoetzel
  * Text Domain: pcs-vc-integration
@@ -23,6 +23,12 @@ if( !class_exists( 'PCS_VC_Integration' ) ) {
 class PCS_VC_Integration {
 
     /**
+     * @since 1.1.0
+     * @var   string Text domain used for translations
+     */
+    CONST TEXT_DOMAIN = 'pcs-vc-integration';
+
+    /**
      * @var PCS_VC_Integration $instance The one true PCS_VC_Integration
      * @since       1.0.0
      */
@@ -39,7 +45,7 @@ class PCS_VC_Integration {
         if( !self::$instance ) {
             self::$instance = new PCS_VC_Integration();
             self::$instance->setup_constants();
-//            self::$instance->load_textdomain();
+            self::$instance->load_textdomain();
             self::$instance->hooks();
         }
         return self::$instance;
@@ -54,7 +60,7 @@ class PCS_VC_Integration {
      */
     private function setup_constants() {
         // Plugin version
-        define( 'PCS_VC_INTERGATION_VER', '1.0.1' );
+        define( 'PCS_VC_INTERGATION_VER', '1.1.0' );
         // Plugin path
         define( 'PCS_VC_INTERGATION_DIR', plugin_dir_path( __FILE__ ) );
         // Plugin URL
@@ -76,6 +82,35 @@ class PCS_VC_Integration {
     }
 
     /**
+     * Internationalization
+     *
+     * @access      public
+     * @since       1.1.0
+     * @return      void
+     */
+    public function load_textdomain() {
+        // Set filter for language directory
+        $lang_dir = PCS_VC_INTEGRATION_DIR . '/languages/';
+        $lang_dir = apply_filters( 'pcs_vc_integration_languages_directory', $lang_dir );
+        // Traditional WordPress plugin locale filter
+        $locale = apply_filters( 'plugin_locale', get_locale(), self::TEXT_DOMAIN );
+        $mofile = sprintf( '%1$s-%2$s.mo', self::TEXT_DOMAIN, $locale );
+        // Setup paths to current locale file
+        $mofile_local   = $lang_dir . $mofile;
+        $mofile_global  = WP_LANG_DIR . '/' . self::TEXT_DOMAIN . '/' . $mofile;
+        if( file_exists( $mofile_global ) ) {
+            // Look in global /wp-content/languages/pcs-vc-integration/ folder
+            load_textdomain( self::TEXT_DOMAIN, $mofile_global );
+        } elseif( file_exists( $mofile_local ) ) {
+            // Look in local /wp-content/plugins/pcs-vc-integration/languages/ folder
+            load_textdomain( self::TEXT_DOMAIN, $mofile_local );
+        } else {
+            // Load the default language files
+            load_plugin_textdomain( self::TEXT_DOMAIN, false, 'pcs-vc-integration/languages' );
+        }
+    }
+
+    /**
      * map post-content-shortcodes shortcodes to visual composer elements
      * https://wordpress.org/plugins/post-content-shortcodes/
      *
@@ -86,10 +121,10 @@ class PCS_VC_Integration {
     public function vcMap() {
         // post-content
         vc_map( array(
-            'name' => 'Post Content',
+            'name' => __('Post Content', self::TEXT_DOMAIN),
             'base' => 'post-content',
-            'category' => 'Content',
-            'description' => 'Display the content of one post within another post.',
+            'category' => __('Content', self::TEXT_DOMAIN),
+            'description' => __('Display the content of one post within another post.', self::TEXT_DOMAIN),
             'params' => array(
                 self::idParam(),
                 self::showTitleParam(),
@@ -103,10 +138,10 @@ class PCS_VC_Integration {
 
         // post-list
         vc_map( array(
-            'name' => 'Post List',
+            'name' => __( 'Post List', self::TEXT_DOMAIN),
             'base' => 'post-list',
-            'category' => 'Content',
-            'description' => 'Display the content of one post within another post.',
+            'category' => __( 'Content', self::TEXT_DOMAIN),
+            'description' => __( 'Display the content of one post within another post.', self::TEXT_DOMAIN),
             'params' => array(
                 self::showExcerptParam(),
                 self::excerptLengthParam(),
@@ -127,8 +162,8 @@ class PCS_VC_Integration {
     protected static function idParam() {
         return array(
             'param_name' => 'id',
-            'heading' => 'Post',
-            'description' => 'Select a post. They are grouped by post type',
+            'heading' => __( 'Post', self::TEXT_DOMAIN),
+            'description' => __( 'Select a post. They are grouped by post type', self::TEXT_DOMAIN),
             'type' => 'autocomplete',
             'settings' => array(
                 'sortable' => true,
@@ -138,7 +173,7 @@ class PCS_VC_Integration {
             ),
             'save_always' => true,
             'admin_label' => true,
-            'group' => 'Data',
+            'group' => __( 'Data', self::TEXT_DOMAIN),
         );
     }
 
@@ -152,12 +187,12 @@ class PCS_VC_Integration {
     protected static function showTitleParam() {
         return array(
             'param_name' => 'show_title',
-            'heading' => 'Display the post title?',
-            'description' => 'Whether or not to show the post title at the top of the content. By default, the title is wrapped in h2 tags.',
+            'heading' => __( 'Display the post title?', self::TEXT_DOMAIN),
+            'description' => __( 'Whether or not to show the post title at the top of the content. By default, the title is wrapped in h2 tags.', self::TEXT_DOMAIN),
             'value' => array( __( 'Yes', 'js_composer' ) => true ),
             'type' => 'checkbox',
             'admin_label' => true,
-            'group' => 'Layout',
+            'group' => __( 'Layout', self::TEXT_DOMAIN),
         );
     }
 
@@ -171,12 +206,12 @@ class PCS_VC_Integration {
     protected static function showExcerptParam() {
         return array(
             'param_name' => 'show_excerpt',
-            'heading' => 'Display an excerpt of the post content?',
-            'description' => 'Determines whether to default to showing the post excerpt instead of the post content (still falls back to post content if the excerpt is not set).',
+            'heading' => __( 'Display an excerpt of the post content?', self::TEXT_DOMAIN),
+            'description' => __( 'Determines whether to default to showing the post excerpt instead of the post content (still falls back to post content if the excerpt is not set).', self::TEXT_DOMAIN),
             'value' => array( __( 'Yes', 'js_composer' ) => "true" ),
             'type' => 'checkbox',
             'admin_label' => true,
-            'group' => 'Layout',
+            'group' => __( 'Layout', self::TEXT_DOMAIN),
         );
     }
 
@@ -191,12 +226,12 @@ class PCS_VC_Integration {
     protected static function excerptLengthParam() {
         return array(
             'param_name' => 'excerpt_length',
-            'heading' => 'Excerpt length [words]',
-            'description' => 'Limit the length of the content/excerpt shown. Specify the maximum number of words (a read more link will automatically be appended to any entries that exceed that limit).',
+            'heading' => __( 'Excerpt length [words]', self::TEXT_DOMAIN),
+            'description' => __( 'Limit the length of the content/excerpt shown. Specify the maximum number of words (a read more link will automatically be appended to any entries that exceed that limit).', self::TEXT_DOMAIN),
             'type' => 'textfield',
             'dependency' => array( 'element' => 'show_excerpt', 'not_empty' => true),
             'admin_label' => true,
-            'group' => 'Layout',
+            'group' => __( 'Layout', self::TEXT_DOMAIN),
         );
     }
 
@@ -210,12 +245,12 @@ class PCS_VC_Integration {
     protected static function showImageParam() {
         return array(
             'param_name' => 'show_image',
-            'heading' => 'Display the featured image with the post?',
-            'description' => 'Determines whether or not to display the featured image (if so, this appears before the content).',
+            'heading' => __( 'Display the featured image with the post?', self::TEXT_DOMAIN),
+            'description' => __( 'Determines whether or not to display the featured image (if so, this appears before the content).', self::TEXT_DOMAIN),
             'value' => array( __( 'Yes', 'js_composer' ) => true ),
             'type' => 'checkbox',
             'admin_label' => true,
-            'group' => 'Featured Image',
+            'group' => __( 'Featured Image', self::TEXT_DOMAIN),
         );
     }
 
@@ -229,12 +264,12 @@ class PCS_VC_Integration {
     protected static function imageWidthParam() {
         return array(
             'param_name' => 'image_width',
-            'heading' => 'Image width',
-            'description' => 'The width, in pixels, to which the featured image should be sized.',
+            'heading' => __( 'Image width', self::TEXT_DOMAIN),
+            'description' => __( 'The width, in pixels, to which the featured image should be sized.', self::TEXT_DOMAIN),
             'type' => 'textfield',
             'dependency' => array( 'element' => 'show_image',' value' => array( "1" ), 'not_empty' => true),
             'admin_label' => true,
-            'group' => 'Featured Image',
+            'group' => __( 'Featured Image', self::TEXT_DOMAIN),
         );
     }
 
@@ -248,12 +283,12 @@ class PCS_VC_Integration {
     protected static function imageHeightParam() {        
         return array(
             'param_name' => 'image_height',
-            'heading' => 'Image height',
-            'description' => 'The height, in pixels, to which the featured image should be sized.',
+            'heading' => __( 'Image height', self::TEXT_DOMAIN),
+            'description' => __( 'The height, in pixels, to which the featured image should be sized.', self::TEXT_DOMAIN),
             'type' => 'textfield',
             'dependency' => array( 'element' => 'show_image',' value' => array( "1" ), 'not_empty' => true),
             'admin_label' => true,
-            'group' => 'Featured Image',
+            'group' => __( 'Featured Image', self::TEXT_DOMAIN),
         );
     }
 
